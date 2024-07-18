624d0bf9551459e2a513a3adb72b063809ceb411
Doran <157047979+Doran-Laurentian@users.noreply.github.com>
2024-07-17 22:21:51 -0400

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let currentOldest = 0;
+    let index = -1;
+
+    for (let i = 0; i < people.length; i++){
+        let age = people[i].yearOfDeath - people[i].yearOfBirth;
+        if (age > currentOldest){
+            currentOldest = age;
+            index = i;
+        }       
+    }
+    return people[index];
 };
-
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
