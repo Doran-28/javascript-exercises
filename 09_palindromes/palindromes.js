fb38f026fa6d381a6b9678ce39f40d2242a00d9e
Doran <157047979+Doran-Laurentian@users.noreply.github.com>
2024-07-17 21:48:04 -0400

    Exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let revStr = str.split("").reverse().join("");
 
+    for (let i = 0; i <= str.length; i++){
+        if (str.charAt(i) != revStr.charAt(i))
+            return false;
+        
+    }
+    return true;
 };
 
 // Do not edit below this line
