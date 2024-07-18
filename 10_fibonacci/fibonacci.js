33442f238f9316ec21d04eaf6c61afde76a6408b
Doran <157047979+Doran-Laurentian@users.noreply.github.com>
2024-07-17 21:50:47 -0400

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    if (val == 1)
+        return 1
+    if (val == 0)
+        return 0
 
+    return fibonacci(val - 1) + fibonacci(val - 2)
 };
 
 // Do not edit below this line
