b8ba1660d7916a328cf9ec81fc3b1640c6c3d3a7
Doran <157047979+Doran-Laurentian@users.noreply.github.com>
2024-07-17 21:35:29 -0400

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(givenStr, num) {
+    let str = "";
+    for (let i = 1; i <= num; i++){
+        str += givenStr;
+    }
+    return str;
 };
 
 // Do not edit below this line
